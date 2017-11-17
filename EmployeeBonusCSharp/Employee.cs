f0bc9a67b89ba5bd8a54c2cafdd18bdf146cac78
Vonetta <vonettastevenson@gmail.com>
2017-11-16 18:44:21 -0800

    Add project files.

---
+++ EmployeeBonusCSharp/Employee.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeBonusCSharp
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public int Experience { get; set; }
+
+        //public void CalculateBonus() {
+        //    if (Experience < 2)
+        //    {
+        //        Console.WriteLine( " 10% bonus for " + Experience +
+        //            " years");
+        //    }
+        //    else if (Experience < 5)
+        //    {
+        //        Console.WriteLine( "15% bonus for " + Experience +
+        //          " years");       
+        //    }
+        //    else if (Experience < 10)
+        //    {
+        //        Console.WriteLine("20% bonus for " + Experience +
+        //       " years");    
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("25% bonus for " + Experience +
+        //       " years");     
+        //    }
+
+        //}
+          
+        //have only one return statement
+        public decimal CalculateBonus() {
+            decimal bonus;
+            if (Experience < 2)
+            {
+                bonus = Salary * 0.1m;
+            }
+            else if (Experience < 5)
+            {
+                bonus = Salary * 0.15m;
+            }
+            else if (Experience < 10)
+            {
+                bonus = Salary * 0.2m;
+            }
+            else {
+                bonus = Salary * 0.25m;
+            }
+            return bonus;
+        }
+    }
+
+}
---
+++ EmployeeBonusCSharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeBonusCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please Enter your full name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please Enter your salary");
+            double salary = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please Enter Years of Experience");
+            int experience = int.Parse(Console.ReadLine());
+
+            Employee myEmployees = new Employee()
+            {
+                Name = name,
+                Salary = salary,
+                Experience = experience
+            };
+
+            myEmployees.CalculateBonus();
+                Console.ReadLine();
+            
+
+        }
+    }
+}
